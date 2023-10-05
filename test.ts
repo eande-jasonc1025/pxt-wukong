e2f3af3f7f6f31734bc30bcdbf4e127e60e83143
eande-jasonc1025 <67285598+eande-jasonc1025@users.noreply.github.com>
2023-10-05 01:58:53 -0700

    23-1005-0200 Remove 'neopixel' and seems to work after download test :)+

--- test.ts
+++ test.ts
@@ -1,3 +1,6 @@
+
+wuKong.setLightMode(wuKong.LightMode.BREATH)
+
 input.onButtonPressed(Button.A, function () {
     wuKong.setAllMotor(100, -100)
 })
@@ -10,5 +13,8 @@ input.onButtonPressed(Button.B, function () {
     wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S4, 90)
     wuKong.setServoAngle(wuKong.ServoTypeList._180,wuKong.ServoList.S6, 0)
 })
-basic.showIcon(IconNames.Heart)
-wuKong.setLightMode(wuKong.LightMode.BREATH)
+basic.forever(function() {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(200)
+    basic.showIcon(IconNames.SmallHeart)
+})
